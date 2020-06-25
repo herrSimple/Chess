293db4e76b1d19c99a4ef2abb7db9dba8d510cc9
herrSimple <54478565+herrSimple@users.noreply.github.com>
2020-06-25 16:27:20 +0100

    Update Minmax.py

--- Main/util/Minmax.py
+++ Main/util/Minmax.py
@@ -1,34 +1,4 @@
-from timeit import timeit
 
-class Test:
-	__foo = [1, 2, 3, 4, 5, 6]
 
-	
-	def foo(self, a, b=lambda a: "positive" if vars()["a"] == 0 else "negative"):
-		print(a, "is", b)
-
-bar = Test()
-bar.foo(10)
-
-a = 4
-
-def on_board(pos):
-        return 7 >= pos[0] >= 0 and 7 >= pos[1] >= 0
-
-LEFT = (0, -1)
-RIGHT = (0, 1)
-
-print(LEFT == (0, -1))
-
-alist = [[a, a] for a in range(8) for b in range(8)]
-moves = []
-
-for a, b in alist:
-	pos = [a-1, b-1]
-	moves.append(pos)
-	pos = [0, 0]
-
-print(moves)
-
-KEY = ("WHITE_KING", (1, 1))
-VALUE = [[0, 0], [2, 1]]
+class Minmax():
+	pass
